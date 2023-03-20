7974bf2af5cb443357b4cab72dd955ec15e71b40
JINNY <sousaca05@gmail.com>
2023-03-20 15:28:42 +0900

    230320_school

--- Method/Program.cs
+++ Method/Program.cs
@@ -6,12 +6,24 @@ namespace Method
     class Calculator
     {
         public static int Plus(int a, int b)
-
-
-
-
-        static void Main(string[] args)
         {
+            return a + b;
+        }
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        class MainApp
+        {
+            public static void Main()
+            {
+                int resuit = Calculator.Plus(3, 4);
+                Console.WriteLine(resuit);
+
+                resuit = Calculator.Minus(5, 2);
+                Console.WriteLine(resuit);
+            }
         }
     }
 }
