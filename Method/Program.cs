1c40426152d6017bf85a8c55c721e60805a8614d
JINNY <sousaca05@gmail.com>
2023-03-21 15:37:08 +0900

    230321_school

--- Method/Program.cs
+++ Method/Program.cs
@@ -5,20 +5,20 @@ namespace Method
 {
     class Calculator
     {
-        public static int Plus(int a, int b)
+        public static int Plus(int a, int b) // a,b 매개변수
         {
             return a + b;
         }
         public static int Minus(int a, int b)
         {
             return a - b;
-        }
+        } 
 
         class MainApp
         {
             public static void Main()
             {
-                int resuit = Calculator.Plus(3, 4);
+                int resuit = Calculator.Plus(3, 4); // 3,4 인수
                 Console.WriteLine(resuit);
 
                 resuit = Calculator.Minus(5, 2);
---
+++ SwapByValue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SwapByValue
+{
+    class Program
+    {
+        public static void Swap(int a, int b, int c)
+        {
+            int temp = b;
+            b= a;
+            a = c;
+            c = temp;
+        }
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 4;
+            int z = 5;
+
+            Console.WriteLine($"x:{x}, y:{y}, z:{z}");
+
+            Swap(x, y,z);
+
+            Console.WriteLine($"x:{x}, y:{y}, z:{z}");
+        }
+    }
+}
