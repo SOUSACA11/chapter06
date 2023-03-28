31eb2531a8c7ec03a72f2ea7b2610a9c90601e07
JINNY <sousaca05@gmail.com>
2023-03-28 13:46:21 +0900

    230328_school

---
+++ Jiiny_ex1/MainApp01.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Jiiny_ex1
+{
+    class MainApp01
+    {
+        static double Square(double arg)
+        {
+            double star = arg * arg;
+            return star;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("수를 입력하세요 : ");
+            string input = Console.ReadLine();
+            double arg = Convert.ToDouble(input);
+
+           // Console.WriteLine("결과 : {0}", Square(arg));
+            Console.WriteLine($"결과 : {Square(arg)}");
+        }
+    }
+}
