1f206212869d40e5c3178051de3c0906c1495c4d
JINNY <sousaca05@gmail.com>
2023-03-30 08:30:28 +0900

    230330_school

--- Jiiny_ex1/MainApp02-1.cs
+++ Jiiny_ex1/MainApp02-1.cs
@@ -1,19 +1,18 @@
 ﻿using System;
 namespace Jiiny_ex1
 {
-    class MainApp02 //out 버전
+    class MainApp02 //ref(참조) 버전
     {
         public static void Main()
         {
             double mean = 0;
-            Mean(1, 2, 3, 4, 5, out mean);
+            Mean(1, 2, 3, 4, 5, ref mean);
             Console.WriteLine("평균 : {0}", mean);
         }
 
-        public static void Mean(double a, double b, double c, double d, double e, out double mean)
+        public static void Mean(double a, double b, double c, double d, double e, ref double mean)
         {
             mean = (a + b + c + d + e) / 5;
         }
     }
 }
-
---
+++ Jiiny_ex1/MainApp03.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Jiiny_ex1
+{
+    class MainApp03
+    {
+        public static void Main()
+        {
+            int a = 3;
+            int b = 4;
+            int resultA = 0;
+
+            Plus(a, b, out resultA);
+
+            Console.WriteLine("{0} + {1} = {2}", a, b, resultA);
+
+            double x = 2.4;
+            double y = 3.1;
+            double resultB = 0;
+
+            Plus(x, y, out resultB); // 오버로드가 필요한 메소드
+
+            Console.WriteLine("{0} + {1} = {2}", x, y, resultB);
+        }
+
+        public static void Plus(int a, int b, out int c)
+        {
+            c = a + b;
+        }
+
+       
+        public static void Plus(double a, double b, out double c)
+        {
+            c = a + b;
+        }
+        
+        
+        // 더블형 매개변수 받을 수 있도록 오버로딩된 플러스 메소드 작성
+
+    }
+}
