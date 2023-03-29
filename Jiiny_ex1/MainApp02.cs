b605a433cd6b80e8d36dc5f63240e97a66f1e8d0
JINNY <sousaca05@gmail.com>
2023-03-29 15:44:01 +0900

    230329_school

--- Jiiny_ex1/MainApp02.cs
+++ Jiiny_ex1/MainApp02.cs
@@ -1,2 +1,19 @@
-﻿using System
+﻿using System;
+namespace Jiiny_ex1
+{
+    class MainApp02
+    {
+        public static void Main()
+        {
+            double mean = 0;
+            Mean(1, 2, 3, 4, 5, out mean);
+            Console.WriteLine("평균 : {0}", mean);
+        }
+
+        public static void Mean(double a, double b, double c, double d, double e, out double mean)
+        {
+            mean = (a + b + c + d + e) / 5;
+        }
+    }
+}
 
